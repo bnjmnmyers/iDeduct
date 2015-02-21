654a30acfea1f6ac13fc4719ac52ed41e66c3361
bnjmnmyers <ben.myers@benjaminmyers.com>
2015-02-21 10:17:16 -0500

    Initial Commit

---
+++ iDeductTests/iDeductTests.swift
@@ -0,0 +1,36 @@
+//
+//  iDeductTests.swift
+//  iDeductTests
+//
+//  Created by Benjamin Myers on 2/21/15.
+//  Copyright (c) 2015 AppGuys. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iDeductTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
